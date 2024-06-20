b5257e43ffd7f268d2174d7a215a502d1677f893
mouryasuraj <mouryasuraj6266444@gmail.com>
2024-06-20 17:51:50 +0530

    add functionality to the next and previous button

--- src/components/AllImages.jsx
+++ src/components/AllImages.jsx
@@ -10,9 +10,12 @@ const AllImages = () => {
   useMovieImages();
   const { movieDetails } = useMovieInfo();
   const { movie_id, movie_name } = useParams();
-  const [isBackdropActive, setIsBackdropActive] = useState(false);
-  const [isLogoActive, setIsLogoActive] = useState(true);
+  const [isBackdropActive, setIsBackdropActive] = useState(true);
+  const [isLogoActive, setIsLogoActive] = useState(false);
   const [isPosterActive, setIsPosterActive] = useState(false);
+  const [backdropIndex, setBackdropIndex] = useState(0);
+  const [logosIndex, setLogosIndex] = useState(0);
+  const [posterIndex, setPosterIndex] = useState(0);
   const goTo = `/movie/${movie_name}/${movie_id}`;
   const allImages = useSelector((store) => store.app.allImages);
   const backdrop = allImages.backdrops;
@@ -38,50 +41,109 @@ const AllImages = () => {
   if (!allImages || !movieDetails) return <p className="pt-32">Loading.....</p>;
 
   return (
-    <div className="pt-[6%] max-w-[1200px] mx-auto">
-      {/* Breadcumbs */}
-      <div className="my-4">
-        <Breadcumbs
-          goTo={goTo}
-          previousPage={movieDetails.title}
-          currentPage="Images"
-        />
-      </div>
-      {/* Images */}
-      {/* Backdrop */}
-      <div className="border-b dark:border-slate-100 border-slate-900">
-        <div className="relative w-fit flex items-center">
-          <h2
-            onClick={handleBackdrop}
-            className={`md:text-xl border-b-4  text-xl font-bold text-slate-900 ${
-              isBackdropActive
-                ? "dark:text-blue-300 text-blue-600 border-text-600  dark:border-blue-300 border-blue-600 "
-                : "dark:text-slate-100 border-transparent"
-            } px-4 py-3  cursor-pointer transition-all duration-500  `}
-          >
-            Backdrops
-          </h2>
-          <h2
-            onClick={handleLogos}
-            className={`md:text-xl border-b-4  text-xl font-bold text-slate-900 ${
-              isLogoActive
-                ? "dark:text-blue-300 text-blue-600 border-text-600  dark:border-blue-300 border-blue-600 "
-                : "dark:text-slate-100 border-transparent"
-            } px-4 py-3  cursor-pointer transition-all duration-500  `}
-          >
-            Logos
-          </h2>
-          <h2
-            onClick={handlePoster}
-            className={`md:text-xl border-b-4  text-xl font-bold text-slate-900 ${
-              isPosterActive
-                ? "dark:text-blue-300 text-blue-600 border-text-600  dark:border-blue-300 border-blue-600 "
-                : "dark:text-slate-100 border-transparent"
-            } px-4 py-3  cursor-pointer transition-all duration-500  `}
-          >
-            Posters
-          </h2>
+    <div className="px-5">
+      <div className="pt-[16%] sm:pt-[13%] md:pt-[8%] lg:pt-[6%] max-w-[1200px] mx-auto">
+        {/* Breadcumbs */}
+        <div className="my-4">
+          <Breadcumbs
+            goTo={goTo}
+            previousPage={movieDetails.title}
+            currentPage="Images"
+          />
         </div>
+        {/* Tabs */}
+        <div className="border-b dark:border-slate-100 border-slate-900">
+          <div className="relative w-fit flex items-center">
+            <h2
+              onClick={handleBackdrop}
+              className={`md:text-xl border-b-4  text-md font-bold text-slate-900 ${
+                isBackdropActive
+                  ? "dark:text-blue-300 text-blue-600 border-text-600  dark:border-blue-300 border-blue-600 "
+                  : "dark:text-slate-100 border-transparent"
+              } px-4 md:py-3 py-2  cursor-pointer transition-all duration-500  `}
+            >
+              Backdrops
+            </h2>
+            <h2
+              onClick={handleLogos}
+              className={`md:text-xl border-b-4  text-md font-bold text-slate-900 ${
+                isLogoActive
+                  ? "dark:text-blue-300 text-blue-600 border-text-600  dark:border-blue-300 border-blue-600 "
+                  : "dark:text-slate-100 border-transparent"
+              } px-4 md:py-3 py-2  cursor-pointer transition-all duration-500  `}
+            >
+              Logos
+            </h2>
+            <h2
+              onClick={handlePoster}
+              className={`md:text-xl border-b-4  text-md font-bold text-slate-900 ${
+                isPosterActive
+                  ? "dark:text-blue-300 text-blue-600 border-text-600  dark:border-blue-300 border-blue-600 "
+                  : "dark:text-slate-100 border-transparent"
+              } px-4 md:py-3 py-2  cursor-pointer transition-all duration-500  `}
+            >
+              Posters
+            </h2>
+          </div>
+        </div>
+        {/* Big Image */}
+        {isBackdropActive && (
+          <div className="flex w-full items-center justify-between py-6 gap-3">
+            <div
+              onClick={() => setBackdropIndex((prev) => prev - 1)}
+              className="text-2xl cursor-pointer dark:bg-slate-200 hover:dark:bg-opacity-80  hover:opacity-90 bg-slate-800 w-fit px-3 py-2 h-fit rounded-md bg-opacity-90 dark:text-slate-900 text-slate-100"
+            >
+              <i className="fa-solid fa-arrow-left"></i>
+            </div>
+            <img
+              className="w-[60%] rounded-lg select-none"
+              src={TMDB_IMAGE + backdrop[backdropIndex].file_path}
+              alt=""
+            />
+            <div
+              onClick={() => setBackdropIndex((prev) => prev + 1)}
+              className="text-2xl cursor-pointer dark:bg-slate-200 hover:dark:bg-opacity-80  hover:opacity-90 bg-slate-800 w-fit px-3 py-2 h-fit rounded-md bg-opacity-90 dark:text-slate-900 text-slate-100"
+            >
+              <i className="fa-solid fa-arrow-right"></i>
+            </div>
+          </div>
+        )}
+        {isLogoActive && (
+          <div className="flex w-full items-center justify-between py-6 gap-3">
+            <div
+              onClick={() => setLogosIndex((prev) => prev - 1)}
+              className="text-2xl cursor-pointer dark:bg-slate-200 hover:dark:bg-opacity-80  hover:opacity-90 bg-slate-800 w-fit px-3 py-2 h-fit rounded-md bg-opacity-90 dark:text-slate-900 text-slate-100"
+            >
+              <i className="fa-solid fa-arrow-left"></i>
+            </div>
+            <img
+              className="w-[30%] rounded-lg select-none"
+              src={TMDB_IMAGE + logos[logosIndex].file_path}
+              alt=""
+            />
+            <div onClick={()=> setLogosIndex(prev => prev +1)} className="text-2xl cursor-pointer dark:bg-slate-200 hover:dark:bg-opacity-80  hover:opacity-90 bg-slate-800 w-fit px-3 py-2 h-fit rounded-md bg-opacity-90 dark:text-slate-900 text-slate-100">
+              <i className="fa-solid fa-arrow-right"></i>
+            </div>
+          </div>
+        )}
+        {isPosterActive && (
+          <div className="flex w-full items-center justify-between py-6 gap-3">
+            <div
+              onClick={() => setPosterIndex((prev) => prev - 1)}
+              className="text-2xl cursor-pointer dark:bg-slate-200 hover:dark:bg-opacity-80  hover:opacity-90 bg-slate-800 w-fit px-3 py-2 h-fit rounded-md bg-opacity-90 dark:text-slate-900 text-slate-100"
+            >
+              <i className="fa-solid fa-arrow-left"></i>
+            </div>
+            <img
+              className="w-[20%] rounded-lg select-none"
+              src={TMDB_IMAGE + poster[posterIndex].file_path}
+              alt=""
+            />
+            <div onClick={()=> setPosterIndex(prev => prev +1)} className="text-2xl cursor-pointer dark:bg-slate-200 hover:dark:bg-opacity-80  hover:opacity-90 bg-slate-800 w-fit px-3 py-2 h-fit rounded-md bg-opacity-90 dark:text-slate-900 text-slate-100">
+              <i className="fa-solid fa-arrow-right"></i>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/Breadcumbs.jsx
+++ src/components/Breadcumbs.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Breadcumbs = ({previousPage, currentPage, goTo}) => {
 
   return (
-    <div className='flex items-center gap-2'>
+    <div className='flex items-center md:gap-5 gap-3'>
       <Link to={goTo} className='dark:text-slate-100 font-semibold text-slate-900 cursor-pointer hover:underline '>{previousPage}</Link>
       <i class="fa-solid fa-angle-right dark:text-slate-100 text-slate-900"></i>
       <p className='dark:text-slate-100 text-slate-900 font-semibold'>{currentPage}</p>
